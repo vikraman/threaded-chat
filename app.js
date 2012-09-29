63b1ebac07d7c2d8ea5ca1b06645315a7952dd88
Vikraman Choudhury <vikraman.choudhury@gmail.com>
2012-09-29 23:41:06 +0530

    init

---
+++ app.js
@@ -0,0 +1,28 @@
+
+var http = require('http'),
+    io = require('socket.io'),
+    express = require('express'),
+    crypto = require('crypto');
+
+var app = express();
+app.use(express.static(__dirname + '/static'));
+
+var server = http.createServer(app);
+var port = process.env.PORT || 8000;
+server.listen(port);
+
+var socket = io.listen(server);
+
+var clients = [];
+
+socket.sockets.on('connection', function(client) {
+    clients.push(client);
+    client.on('im', function(im) {
+	crypto.randomBytes(8, function(ex, buf) {
+	    im.id = buf.toString('hex');
+	    clients.forEach(function(client) {
+		client.emit('im', im);
+	    });
+	});
+    });
+});
---
+++ static/client.js
@@ -0,0 +1,39 @@
+
+var socket = io.connect();
+var cur_id = null;
+
+socket.on('msg', function(data) {
+    console.log(data);
+});
+
+socket.on('im', function(im) {
+    var parent_el = im.parent == null ? $('#log') : $('#' + im.parent);
+    parent_el.append('<blockquote><div id=\'' + im.id + '\'></div></blockquote>');
+    var im_id_el = $('#' + im.id);
+    im_id_el.append(new Date(im.dt).toLocaleTimeString() + ': ' + im.text);
+    cur_id = im.id;
+    $('#im').attr('placeholder', im.text);
+    im_id_el.on('click', function(event) {
+	event.stopPropagation();
+	cur_id = im.id;
+	$('#im').attr('placeholder', im.text);
+    });
+});
+
+$(function() {
+    var im_el = $('#im');
+    im_el.on('keypress', function(event) {
+	if(event.keyCode == 13) {
+	    var text = im_el.attr('value');
+	    if(text) {
+		var im = {'dt': new Date(),
+			  'text': text,
+			  'parent': cur_id
+			 };
+		socket.emit('im', im);
+		im_el.attr('value', '');
+		im_el.focus();
+	    }
+	}
+    });
+});
