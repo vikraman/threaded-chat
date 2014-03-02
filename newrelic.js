4be6e102596aad83778854b8e0a9011c0945d415
Vikraman Choudhury <vikraman.choudhury@gmail.com>
2014-03-02 12:44:24 +0530

    Setup new relic agent configuration

---
+++ newrelic.js
@@ -0,0 +1,13 @@
+/**
+ * New Relic agent configuration.
+ *
+ */
+exports.config = {
+
+  app_name : [process.env.NEW_RELIC_APP_NAME],
+
+  license_key : process.env.NEW_RELIC_LICENSE_KEY,
+  logging : {
+    level : 'trace'
+  }
+};
